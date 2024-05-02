d245206a7180e7d145c89fd395a8a593098a7da7
kiart-tantasi <kiarttantasi@gmail.com>
2024-05-02 12:08:12 +0700

    refactor limit-goroutine

--- goroutine/limit-goroutine/main.go
+++ goroutine/limit-goroutine/main.go
@@ -7,22 +7,25 @@ import (
 
 func main() {
 	resultAmount := 9
-
-	limiter := make(chan int, 3)
+	limiter := make(chan int, 3) // ony allow 3 goroutines to run in parallel
 	results := make(chan string, resultAmount)
+
+	// === these 2 for-loop will run in parellel at the end because the first one is executing goroutines ===
 	for i := 1; i <= resultAmount; i++ {
+		// reserve the space in limiter channel to make other goroutine wait
 		limiter <- 0
 		go doTask(i, results, limiter)
 	}
 	for i := 1; i <= resultAmount; i++ {
+		// pull value from result channel
 		fmt.Println("result:", <-results)
 	}
 }
 
 func doTask(taskNum int, results chan string, limiter chan int) {
-	fmt.Println("started task", taskNum)
 	time.Sleep(3 * time.Second)
-	results <- fmt.Sprintf("hello world %d", taskNum)
-	fmt.Println("finished task", taskNum)
+	results <- fmt.Sprintf("result for task no. %d", taskNum)
+	fmt.Println("finished task no.", taskNum)
+	// pull value from channel (free the space for next task)
 	<-limiter
 }
