d0a4b4f424b2287c7725ecf9765c747648ea69d4
kiart-tantasi <kiarttantasi@gmail.com>
2024-05-02 12:45:58 +0700

    refactor limit-goroutine/main.go

--- goroutine/limit-goroutine/main.go
+++ goroutine/limit-goroutine/main.go
@@ -2,30 +2,58 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 )
 
 func main() {
-	resultAmount := 9
-	limiter := make(chan int, 3) // ony allow 3 goroutines to run in parallel
-	results := make(chan string, resultAmount)
-
-	// === these 2 for-loop will run in parellel at the end because the first one is executing goroutines ===
-	for i := 1; i <= resultAmount; i++ {
-		// reserve the space in limiter channel to make other goroutine wait
-		limiter <- 0
-		go doTask(i, results, limiter)
-	}
-	for i := 1; i <= resultAmount; i++ {
-		// pull value from result channel
-		fmt.Println("result:", <-results)
-	}
+	limitDemo()
+	// limitDeadlockDemo()
 }
 
-func doTask(taskNum int, results chan string, limiter chan int) {
-	time.Sleep(3 * time.Second)
-	results <- fmt.Sprintf("result for task no. %d", taskNum)
-	fmt.Println("finished task no.", taskNum)
-	// pull value from channel (free the space for next task)
-	<-limiter
+// this demo is complex but in short, goroutine has magic:
+// goroutine has blocking behavior when channel is full
+
+func limitDemo() {
+	limit := 2
+	limiter := make(chan int, limit)
+	resultAmount := 4
+	results := make(chan int, resultAmount)
+
+	for num := 1; num <= resultAmount; num++ {
+		// reserve space in limit channel
+		// because we have code to free space in function below so we can reserve space in limiter channel with more then limit
+		limiter <- 0
+		fmt.Println("done, reserved limit for number", num)
+		currentNum := num
+		go func() {
+			time.Sleep(2 * time.Second)
+			results <- currentNum
+			// free space
+			<-limiter
+			fmt.Println("freed space for number", currentNum)
+		}()
+	}
+	// hack to wait for all goroutines to finish
+	time.Sleep(10 * time.Second)
+}
+
+func limitDeadlockDemo() {
+	limit := 2
+	limiter := make(chan int, limit)
+	resultAmount := 4
+	results := make(chan string, resultAmount)
+
+	for num := 1; num <= resultAmount; num++ {
+		limiter <- 0
+		currentNum := num
+		go func() {
+			time.Sleep(2 * time.Second)
+			str := strconv.Itoa(currentNum)
+			results <- str
+			// we never promise to free space in limiter channel
+			// so deadlock error wiil be throwned
+			// <-limiter // un-comment to promise to free space to prevent deadlock error
+		}()
+	}
 }
