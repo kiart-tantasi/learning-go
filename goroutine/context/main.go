9801a522e4c0a2edc577ca796ad20f474cc0d0fa
kiart-tantasi <kiarttantasi@gmail.com>
2024-05-12 20:08:16 +0700

    create fn demoWithTimeout

--- goroutine/context/main.go
+++ goroutine/context/main.go
@@ -1,26 +1,36 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 )
 
-// TODO: use context package to help cancel unused goroutine
-
 func main() {
-	channel := make(chan string)
-	go func() {
-		time.Sleep(3 * time.Second)
-		fmt.Println("goroutine is finished")
-		channel <- "value"
-	}()
+	demoWithTimeout()
+}
 
-	select {
-	case val := <-channel:
-		fmt.Println(val)
-	case <-time.After(2 * time.Second):
-		fmt.Println("timeout")
-		time.Sleep(3 * time.Second)
-		return
-	}
+func demoWithTimeout() {
+	channel := make(chan string)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel() // prevent resource leak
+
+	go func(ctx context.Context, channel chan string) {
+		for {
+			select {
+			// timeout
+			case <-ctx.Done():
+				channel <- ctx.Err().Error()
+				return
+			// pretend that job is done after 5 seconds which already exceeds timeout
+			// you can set it to 1 second so the job is done before timeout
+			case <-time.After(5 * time.Second):
+				channel <- "job is done before timeout, yeahhh!"
+				return
+			}
+		}
+	}(ctx, channel)
+
+	// let's check result
+	fmt.Println("result:", <-channel)
 }
