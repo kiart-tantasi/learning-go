ecec420a224a70e86aaa41e74dec45c274889d42
kiart-tantasi <kiarttantasi@gmail.com>
2025-01-07 13:05:24 +0700

    simplify close-channel-2

--- goroutine/close-channel-2/main.go
+++ goroutine/close-channel-2/main.go
@@ -9,22 +9,24 @@ import (
 // the way to avoid it is to use syntax "val,ok := <- ch" or to leave channel opened
 
 func main() {
-	valueFromClosedChannel()
+	getValueFromClosedChannel()
 }
 
-func valueFromClosedChannel() {
+func getValueFromClosedChannel() {
 	ch := make(chan int)
 	go func() {
+		defer close(ch) // comment this line and you will see the difference (we will not get values from being-opened channel)
 		for i := 0; i < 3; i++ {
 			ch <- i
 		}
-		close(ch) // comment this line and you will see the difference
 	}()
 
 	for {
 		select {
+		// get a value
 		case val := <-ch:
 			fmt.Println("val:", val)
+		// no value to get
 		default:
 			fmt.Println("sleeping...")
 			time.Sleep(1 * time.Second)
