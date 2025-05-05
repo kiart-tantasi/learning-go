b4d947f03d14f5a366d05215c99e9a6a133e492c
ktantasi_stt <ktantasi@smartertravel.com>
2025-05-05 14:34:54 +0700

    refactor fan-out

--- fan-in-fan-out/fan-out/main.go
+++ fan-in-fan-out/fan-out/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // summary: single producer with multiple consumers
 
@@ -14,28 +16,42 @@ func main() {
 		close(ch)
 	}(producer)
 
-	switcher := false
-	ch1 := make(chan int)
-	ch2 := make(chan int)
+	// ch1 := make(chan int)
+	// ch2 := make(chan int)
 
 	// use for-loop to distribute values from a single channel to multiple goroutines
-	for val := range producer {
-		if switcher {
-			// consumer 1
-			go func() {
-				fmt.Println("consumer 1's value:", val)
-				ch1 <- val
-			}()
-		} else {
-			// consumer 2
-			go func() {
-				fmt.Println("consumer 2's value:", val)
-				ch2 <- val
-			}()
+	run := true
+	for run {
+		select {
+		case val, ok := <-producer:
+			// skip if retrieving invalid value from channel
+			if !ok {
+				continue
+			}
+
+			if isConsumer1(val) {
+				// consumer 1
+				go func() {
+					fmt.Println("consumer 1's value:", val)
+					// ch1 <- val
+				}()
+			} else {
+				// consumer 2
+				go func() {
+					fmt.Println("consumer 2's value:", val)
+					// ch2 <- val
+				}()
+			}
+
+		default:
+			// no values found at the moment
 		}
-		// change switch
-		switcher = !switcher
 	}
 
-	// we can also do fan-in from ch1 and ch2 here
+	// we can also do fan-in from ch1 and ch2 here (code for ch1 and ch2 is commented aboce)
+}
+
+func isConsumer1(val int) bool {
+	// get all odd numbers
+	return val%2 != 0
 }
