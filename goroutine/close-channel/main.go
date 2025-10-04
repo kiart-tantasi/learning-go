780bbc6d4481ee7a619dae6be9d03376f7348a57
ktantasi_stt <ktantasi@smartertravel.com>
2025-10-04 13:49:17 +0700

    refactor close-channel dir

--- goroutine/close-channel/main.go
+++ goroutine/close-channel/main.go
@@ -3,14 +3,43 @@ package main
 import "fmt"
 
 // issue: https://stackoverflow.com/a/67422367/21331113
-// in short: range over terminates only when channel is closed
+// in short: range-over terminates only when channel is closed
 
 func main() {
-	// noDeadlock()
 	// hasDeadlock()
+	noDeadlock1()
+	// noDeadlock2()
 }
 
-func noDeadlock() {
+func hasDeadlock() {
+	ch := make(chan int)
+	go func() {
+		ch <- 1
+	}()
+	for val := range ch {
+		fmt.Println(val)
+	}
+}
+
+func noDeadlock1() {
+	ch := make(chan int)
+	go func() {
+		ch <- 1
+		defer close(ch) // fix (close channel so range-over does not get deadlock)
+	}()
+	for val := range ch {
+		fmt.Println(val)
+	}
+
+	// check channel status after it is closed
+	if _, ok := <-ch; ok {
+		fmt.Println("Channel is open")
+	} else {
+		fmt.Println("Channel is closed")
+	}
+}
+
+func noDeadlock2() {
 	ch := make(chan int)
 	go func() {
 		ch <- 1
@@ -18,16 +47,5 @@ func noDeadlock() {
 	// alternatively, you can use this classic for-loop
 	for i := 0; i < 1; i++ {
 		fmt.Println(<-ch)
-	}
-}
-
-func hasDeadlock() {
-	ch := make(chan int)
-	go func() {
-		// defer close(ch) // <-- one of the ways to fix this issue
-		ch <- 1
-	}()
-	for val := range ch {
-		fmt.Println(val)
 	}
 }
